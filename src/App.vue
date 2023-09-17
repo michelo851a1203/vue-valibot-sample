b160d7f690f70a17ddb910f45da2573d6d660e52
MichaelHo <michelo851a1203@gmail.com>
2023-09-17 21:23:06 +0800

    - remove: remove unncessary
      # 2023-09-17-002
    
    	修改：     src/App.vue
    	刪除：     src/components/BaseInput.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,10 @@
 <script setup lang="ts">
-  import { ref } from 'vue';
-  import BaseInputVue from './components/BaseInput.vue';
-
-  const inputRef = ref('');
-
-  // const inputFunction = useDebounceFn((val) => {
-  //   console.log(val)
-  // }, 1000)
-  // watch(inputRef, inputFunction)
-
 </script>
 
 <template>
-  <section mt-32>
-    <BaseInputVue w-32 :id="12" label="test" v-model="inputRef" />
+  <section w-full h-screen flex items-center justify-center box-border>
+    <LoginForm />
   </section>
-  {{ inputRef }}
-  {{ typeof inputRef }}
-
-  <!-- <section w-full h-screen flex items-center justify-center box-border> -->
-  <!--   <LoginForm /> -->
-  <!-- </section> -->
 </template>
 <style>
   body {
